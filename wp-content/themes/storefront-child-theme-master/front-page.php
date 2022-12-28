8dc1c024e2d3dbc4a965e8599bfa2b49f504586a
Diego Granados <diego@ceicom.com.br>
2022-12-28 17:43:52 -0400

    adicionar cabeçalho e rodapé
    Adicionado os arquivos .php correspondentes ao rodapé e cabeçalho da vitrine, começou a editar front-page.php

--- wp-content/themes/storefront-child-theme-master/front-page.php
+++ wp-content/themes/storefront-child-theme-master/front-page.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php wp_head(); ?>
-</head>
-<body>
-    <header>
-        <nav>
-            <ul>
-                <a href=""><li> HOME</li></a>
-            </ul>
-        </nav>
-    </header>
-    <h1>Hola Soy <?php echo $name; ?></h1>
-</body>
-</html>
+<?php get_header(); ?>
+    <div id="<?php echo get_post()->post_name  ?>">
+        
+    </div>
+<?php get_footer(); ?>
