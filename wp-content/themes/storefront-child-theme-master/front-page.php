569787a4debe8bfce7765278af70f5f004396c30
Diego Granados <diego@ceicom.com.br>
2023-01-04 13:24:35 -0400

    Começando a estruturar o slider home

--- wp-content/themes/storefront-child-theme-master/front-page.php
+++ wp-content/themes/storefront-child-theme-master/front-page.php
@@ -1,5 +1,27 @@
 <?php get_header(); ?>
     <div id="<?php echo get_post()->post_name  ?>">
-        
+        <div class="main-container-prueba">
+            <div class="slider-container">
+                <!-- Swiper -->
+                <div class="swiper mySwiper">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">Slide 1</div>
+                    <div class="swiper-slide">Slide 2</div>
+                    <div class="swiper-slide">Slide 3</div>
+                    <div class="swiper-slide">Slide 4</div>
+                    <div class="swiper-slide">Slide 5</div>
+                    <div class="swiper-slide">Slide 6</div>
+                    <div class="swiper-slide">Slide 7</div>
+                    <div class="swiper-slide">Slide 8</div>
+                    <div class="swiper-slide">Slide 9</div>
+                </div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+                </div>
+            </div>
+            <div class="mision">
+
+            </div>
+        </div>
     </div>
 <?php get_footer(); ?>
